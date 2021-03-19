3e1f7f40747f093b55af6494643c587ce94f4c97
Akhilmj24 <inboxakhil321@gmail.com>
2021-03-19 22:08:22 +0530

    emoji

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const closedFace = document.querySelector(".closed");
 const openFace = document.querySelector(".open");
-const opentext = document.querySelector(".text");
+
 
 closedFace.addEventListener("click", () => {
   if (openFace.classList.contains("open")) {
     openFace.classList.add("active");
-    opentext.classList.add("active");
+    
     closedFace.classList.remove("active")
   }
 });
@@ -14,6 +14,7 @@ openFace.addEventListener("click", () => {
   if (closedFace.classList.contains("closed")) {
     closedFace.classList.add("active");
     openFace.classList.remove("active")
-    opentext.classList.remove("active")
+   
   }
 });
+
